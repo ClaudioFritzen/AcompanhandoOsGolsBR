e1d603fe69bc853cd89c6327ab23886cdac83181
ClaudioFritzen <sdfritzen96@gmail.com>
2022-10-27 15:44:43 +0100

    aparecendo o resultado em tempo real, fazendo alterações manualmente no bd

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,24 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.12.1/firebase
 
 import { doc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
 
-const db = getFirestore(app);
+const db = getFirestore(app)
 
-const unsub = onSnapshot(doc(db, "matches", "br01"), (doc) => {
-    console.log("Current data: ", doc.data());
-});
+
+/*  modificando o placar  */ 
+/* function updateScore(id, br, other) {
+    const element = document.getElementById(id)
+    element.innerText = `${br} x ${other}` 
+
+} */
+
+onSnapshot(doc(db, 'matches', 'br01'), (doc) => {
+    console.log("Current data:", doc.data());
+})
 
 
 function showConfet(id) {
     const element = document.getElementById(id)
     party.confetti(element)
 }
+
+
